45c869473f6b5a235a94ce3dab2abacae036b2fc
berk yalnızcık <byalnzck@gmail.com>
2022-01-10 12:07:01 +0300

    SplashScreenActivity added, UI improved and done

---
+++ app/src/main/java/berk/bilgiyarismasi/ui/activity/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package berk.bilgiyarismasi.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import berk.bilgiyarismasi.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler(mainLooper).postDelayed({
+            Intent(this, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(this)
+                finish()
+            }
+        }, 3000L)
+    }
+}
