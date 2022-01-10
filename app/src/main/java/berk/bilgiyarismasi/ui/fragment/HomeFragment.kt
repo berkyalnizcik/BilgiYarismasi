f20e5581a1129fef94d25e2b208b4e2b41a62e09
berk yalnızcık <byalnzck@gmail.com>
2022-01-10 09:00:30 +0300

    HomeFragment UI designed. Font,Dimens,Colors and Strings added

--- app/src/main/java/berk/bilgiyarismasi/ui/fragment/HomeFragment.kt
+++ app/src/main/java/berk/bilgiyarismasi/ui/fragment/HomeFragment.kt
@@ -1,5 +1,21 @@
 package berk.bilgiyarismasi.ui.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import berk.bilgiyarismasi.R
 import berk.bilgiyarismasi.databinding.FragmentHomeBinding
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate)
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.btnStart.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_questionListFragment)
+        }
+    }
+}
