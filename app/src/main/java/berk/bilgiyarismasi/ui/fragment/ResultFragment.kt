ff1a5c058b9b57d4f1fe3685268b5726426bb56a
berk yalnızcık <byalnzck@gmail.com>
2022-01-11 00:27:49 +0300

    ResultFragment finished with all functions

--- app/src/main/java/berk/bilgiyarismasi/ui/fragment/ResultFragment.kt
+++ app/src/main/java/berk/bilgiyarismasi/ui/fragment/ResultFragment.kt
@@ -1,6 +1,32 @@
 package berk.bilgiyarismasi.ui.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import berk.bilgiyarismasi.R
 import berk.bilgiyarismasi.databinding.FragmentResultBinding
 
+class ResultFragment : BaseFragment<FragmentResultBinding>(FragmentResultBinding::inflate) {
+    private val args: ResultFragmentArgs by navArgs()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showResults()
+        onClick()
+    }
 
-class ResultFragment : BaseFragment<FragmentResultBinding>(FragmentResultBinding::inflate)
+    private fun showResults() {
+        val totalRightAnswerCount = args.rightAnswers
+        val totalFalseAnswerCount = args.falseAnswers
+        with(binding) {
+            tvRightAnswers.text = totalRightAnswerCount.toString()
+            tvFalseAnswers.text = totalFalseAnswerCount.toString()
+        }
+    }
+
+    private fun onClick() {
+        binding.btnBackToQuestion.setOnClickListener {
+            findNavController().navigate(R.id.action_resultFragment_to_questionListFragment)
+        }
+    }
+}
